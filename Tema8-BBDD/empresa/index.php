90ae0f8413f75377fa42aca0b931fff3c5509364
anaqu <anaquero1411@gmail.com>
2025-01-10 10:13:30 +0100

    actualizacion

--- Tema8-BBDD/empresa/index.php
+++ Tema8-BBDD/empresa/index.php
@@ -13,16 +13,67 @@ require_once 'config/config.php';
 require_once 'lib/conexion.php';
 
 $conexion =new Conexion();
+$pdo = $conexion->getPdo();
 
+// Formulario de Registro
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register'])) {
+    // Compruebo que el email es válido
+    $email = filter_var(trim($_POST['email_register']), FILTER_VALIDATE_EMAIL);
+    // Quito los espacios en blanco al comienzo y final de la contraseña
+    $password = trim($_POST['password_register']);
 
-try {
-    $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Conexion realizada con éxito";
-} catch (PDOException $e) {
-    die("Error al conectar con la base de datos: ". $e->getMessage());
+    if ($email && $password) {
+        $stmt = $pdo->prepare("SELECT id FROM usuarios WHERE email = :email");
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        // Si no existe el email en la base de datos, se registra
+        if ($stmt->rowCount() == 0) {
+            $password_hash = password_hash($password, PASSWORD_BCRYPT);
+            // INSERT INTO tabla (columna1, columna2, ...) VALUES (valor1, valor2, ...);
+            $stmt = $pdo->prepare("INSERT INTO usuarios (email, password_hash) VALUES (:email, :password_hash)");
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password_hash', $password_hash);
+            $stmt->execute();
+            echo "Registro exitoso!";
+        } else {
+            echo "El email ya está registrado.";
+        }
+    } else {
+        echo "Por favor completa todos los campos de registro.";
+    }
 }
 
+// Formulario de Inicio de Sesión
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login']) && $_SESSION['errorInicioSesion'] < 3) {
+    $email = filter_var(trim($_POST['email_login']), FILTER_VALIDATE_EMAIL);
+    $password = trim($_POST['password_login']);
+
+    if ($email && $password) {
+        //$stmt = $pdo->prepare("SELECT password_hash FROM usuarios WHERE email = :email");
+        $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        if ($stmt->rowCount() == 1) {
+            $user = $stmt->fetch();
+            if (password_verify($password, $user['password_hash'])) {
+                $_SESSION['errorInicioSesion'] = 0;
+                $_SESSION['rol'] = $user['rol'];
+                $_SESSION['nombre'] = $user['email'];
+                header("Location: bienvenida.php");
+                exit();
+            } else {
+                $_SESSION['errorInicioSesion']++;
+                $_SESSION['ultimoIntento'] = time(); // Registro la hora del último intento fallido
+            }
+        } else {
+            echo "Email no registrado.";
+        }
+    } else {
+        echo "Por favor completa todos los campos de inicio de sesión.";
+    }
+}
 
 ?>
 
